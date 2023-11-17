2d43c88466526d27b7f69b21561d1adc41410414
Julio Ucharima <julio_ucharima@usmp.pe>
2023-11-17 12:33:14 -0500

    add type alias

---
+++ src/02-type-alias.ts
@@ -0,0 +1,25 @@
+// TYPE ALIAS
+//Note: Los types alias siempre se escriben en PascalCase
+type Hero = {
+  name: string;
+  age: number;
+};
+
+let hero: Hero = {
+  name: 'Thor',
+  age: 1500,
+};
+
+// function createHero(name: string, age: number): Hero {
+//   return { name, age };
+// }
+
+// const spiderman = createHero('Spiderman', 8);
+
+//Note: Tambien podemos pasar el tipo Hero como parametro
+function createHero(hero: Hero): Hero {
+  const { name, age } = hero;
+  return { name, age };
+}
+
+const spiderman = createHero({ name: 'Spiderman', age: 18 });
